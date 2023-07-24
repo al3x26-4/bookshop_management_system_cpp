e98bbd66ff0a24a0da95516ffe338314727f1915
Mutale Musona <musonamutale@gmail.com>
2023-07-24 18:47:19 +0200

    formatted code

--- main.cpp
+++ main.cpp
@@ -182,17 +182,6 @@ void writeBookToJson(const char *json_file_directory)
 }
 int main()
 {
-  /* This is an excellent idea to upgrade your skills in C++.
-  All you require to do is build an application that will let
-  its customers add and display books. Furthermore, it will
-  help the customers to change their books by implementing modifications
-  to them or adding or deleting pages. Create an application
-  that will be user-friendly and effortless to access and use.
-  It also lets users check and buy their favorite books anytime,
-  anywhere with one click. But you have to be very mindful of
-  your graphics as it is the major part of your bookshop
-  management system application. */
-
   char currency = '$';
   const char *json_file_directory = "books.json";
 
